ddec8c9f0965ab214f4c3764527101550fbd6ff7
Konrad Geller <konrad.geller@rwth-aachen.de>
2024-04-24 13:58:31 +0200

    add base url for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfil
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/game-library/',
   plugins: [
     vue(),
     NodeGlobalsPolyfillPlugin({
