24a40298d752d22da47aaedb50379e5dbbb1a030
Konrad Geller <konrad.geller@rwth-aachen.de>
2024-04-24 14:26:29 +0200

    add base path

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfil
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://konradge.github.io/game-library/',
   plugins: [
     vue(),
     NodeGlobalsPolyfillPlugin({
